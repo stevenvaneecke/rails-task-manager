edbf0dd3d9a194b449e67913a5d67579a64ae057
steven van eecke <stevenvaneecke@hotmail.com>
2018-11-02 14:39:56 +0800

    tasks completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :tasks
+  #INDEX
+  get 'tasks', to: 'tasks#index', as: :tasks
+  #SHOW 1 TASK
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  #CREATE
+  #get the from
+  #post the from
+  post 'tasks', to: 'tasks#create'
+
+  #UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  #DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
